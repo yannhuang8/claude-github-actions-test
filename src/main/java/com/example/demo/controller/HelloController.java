fe01bbda071573718e94eddaa1ceb9d70326eedf
黄彦雄 <huangyanxiong@vivo.com>
2025-08-04 17:38:35 +0800

    提交测试文件

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to Spring Boot Demo Application!";
+    }
+}
